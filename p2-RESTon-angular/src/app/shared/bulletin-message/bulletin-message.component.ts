5db53fc45c3fbb3aaca312d0d063ec817b0f9824
George Yoo <gcyoosf@gmail.com>
2021-02-01 16:43:19 -0500

    alters date format of bulletin board

--- p2-RESTon-angular/src/app/shared/bulletin-message/bulletin-message.component.ts
+++ p2-RESTon-angular/src/app/shared/bulletin-message/bulletin-message.component.ts
@@ -21,7 +21,7 @@ export class BulletinMessageComponent implements OnInit {
 
   formatDate(date: Date) {
 
-    return moment(date).format('DD/MM/YYYY hh:mm A');
+    return moment(date).format('MM/DD/YYYY hh:mm A');
   }
 
 }
